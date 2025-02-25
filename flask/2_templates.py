4f2c9856d13d1cc4ea9282862a86082dbc9033c2
Shivank Tripathi <st.is.champion@gmail.com>
2025-02-25 22:03:00 +0530

    get, post requests done

--- flask/2_templates.py
+++ flask/2_templates.py
@@ -2,17 +2,17 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask("__name__")
 
-@app.route("/<name>")
-def home(name):
-    return render_template("index1.html", content = name)
-
-@app.route("/for")
-def fo():
+@app.route("/")
+def home():
     return render_template("index1.html")
+
+@app.route("/<name>")
+def home1(name):
+    return render_template("index1.html", content = name)
 
 @app.route("/list")
 def list():
     return render_template("index1.html", list1 = ["q", "w", "e", "r", "t", "y"])
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
